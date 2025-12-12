c8a5c683403a207b9ed5872b66eb04c622e9f14c
Dharmendra Mahilang <161195208+Dharmendra110@users.noreply.github.com>
2025-12-12 11:30:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,32 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+  const prices = document.querySelectorAll(".price");
+
+  let total = 0;
+
+  prices.forEach(p => {
+    total += Number(p.textContent);
+  });
+
+  const table = document.querySelector("table");
+
+  const oldRow = document.getElementById("totalRow");
+  if (oldRow) oldRow.remove();
+
+  const newRow = document.createElement("tr");
+  newRow.id = "totalRow";
+
+  const totalLabel = document.createElement("td");
+  totalLabel.textContent = "Total Price";
+
+  const totalValue = document.createElement("td");
+  totalValue.textContent = total;
+
+  newRow.appendChild(totalLabel);
+  newRow.appendChild(totalValue);
+
+  table.appendChild(newRow);
 };
 
 getSumBtn.addEventListener("click", getSum);
-
